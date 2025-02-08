8593c56f4e21945f0eb95ecedfed1eb45878a277
web-devhub <78843896+CodX-eb@users.noreply.github.com>
2025-02-08 09:11:00 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,90 @@
-$(document).ready(function(){
-	$(window).scroll(function(){
-		// sticky navbar on scroll script
-		if(this.scrollY > 20){
-			$('.navbar').addClass('sticky');
-		}else{
-			$('.navbar').removeClase('sticky');
-		}
+$(document).ready(function () {
+  $(window).scroll(function () {
+    //  sticky navbar on scroll script  //
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
 
-		//scroll-up button show/hide script
-		if(this.scrollY > 500){
-			$('.scroll-up-btn').addClass('show');
-		}else{
-			$('.scroll-up-btn').removeClase('show');
-		}
-	});
+    //  scroll-up button show/hide script  //
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-	//slide-up script
-	$('.scroll-up-btn').click(function(){
-		$('html').animate({ scrollTop: 0});
-		// removing smooth scroll on slide-up button click
-		$('html').css('scrollBehavior', 'auto');
-	});
+  //  slide-up script  //
 
-	$('.navbar .menu li a').click(function(){
-		//smooth scroll on menu items click
-		$('html').css('scrollBehavior', 'smooth');
-	});
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    //  removing smooth scroll on slide-up button click  //
+    $("html").css("scrollBehavior", "auto");
+  });
 
-	//toggle navbar
-	$('.menu-btn').click(function(){
-		$('.navbar .menu').toggleClass('active');
-		$('.menu-btn i').toggleClass('active');
-	});
+  $(".navbar .menu li a").click(function () {
+    //  Smooth scroll on Menu Items click  //
 
-	//typing text animation
-	var typed = new Typed('.typing', {
-		strings: [
-				"Fullstack Developer",
-				"Software Developer",
-				"Python Developer",
-				"Founder",
-				"Author",
-			],
-		typeSpeed: 100,
-		backSpeed: 60,
-		loop: true
-	});
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-	var typed = new Typed('.typing-2', {
-		strings: [
-				"Fullstack Developer",
-				"Software Developer",
-				"Python Developer",
-				"Founder",
-				"Author",
-			],
-		typeSpeed: 100,
-		backSpeed: 60,
-		loop: true
-	});
+  //  Toggle Navbar  //
 
-	//owl carousel
-	$('.carousel').owlCarousel({
-		margin: 20,
-		loop: true,
-		autoplay: true,
-		autoplayTimeOut: 2000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-				nav: false
-			},
-			600: {
-				items: 2,
-				nav: false
-			},
-			1000: {
-				items: 3,
-				nav: false
-			}
-		}
-	});
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  //  Typing Text Animation  //
+
+  var typed = new Typed(".typing", {
+    strings: [
+      "Fullstack Developer",
+      "Software Developer",
+      "Python Developer",
+      "Founder",
+      "Author"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "Fullstack Developer",
+      "Software Developer",
+      "Python Developer",
+      "Founder",
+      "Author"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  //  Owl Carousel  //
+
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
+    }
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
